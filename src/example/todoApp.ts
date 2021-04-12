7ab4b40b3f887b5117dd495b9dfb5a35e12c5532
Danilo F. Silva <danilofes@gmail.com>
2021-04-12 20:10:35 -0300

    Make the code prettier

--- src/example/todoApp.ts
+++ src/example/todoApp.ts
@@ -6,10 +6,11 @@ interface ITask {
 };
 
 export function mountTodoApp(rootEl: HTMLElement) {
-  const taskDescription = state("");
-  const tasks = state<ITask[]>([]);
-  const hash = locationHashState();
-  const pendingCount = compute(tasks, tasks => tasks.filter(task => !task.done).length);
+  const
+    taskDescription = state(""),
+    tasks = state<ITask[]>([]),
+    hash = locationHashState(),
+    pendingCount = compute(tasks, tasks => tasks.filter(task => !task.done).length);
 
   function addTask() {
     const newTask = { done: false, description: taskDescription.getValue() };
@@ -23,35 +24,32 @@ export function mountTodoApp(rootEl: HTMLElement) {
 
   root(rootEl).children(
     el.form({ onSubmit: evt => { evt.preventDefault(); addTask() } },
-
       el.inputText({ placeholder: "What needs to be done?", model: taskDescription }),
-
       el.button({ disabled: compute(taskDescription, taskDescription => !taskDescription) }, "Add task"),
+    ),
 
-      el.ul(
-        $repeat(tasks, (task, i) => {
-          const isVisible = compute([task.$.done, hash], (done, hash) => !(done && hash === '#/active' || !done && hash === '#/completed'));
-
-          return $if(isVisible, () =>
-            el.li(
-              el.inputCheckbox({ model: task.$.done }),
-              text(task.$.description),
-              el.button({ type: "button", onClick: deleteTask(i) }, "Delete task")
-            )
+    el.ul(
+      $repeat(tasks, (task, i) => {
+        const isVisible = compute([task.$.done, hash], (done, hash) => !(done && hash === '#/active' || !done && hash === '#/completed'));
+        return $if(isVisible, () =>
+          el.li(
+            el.inputCheckbox({ model: task.$.done }),
+            text(task.$.description),
+            el.button({ type: "button", onClick: deleteTask(i) }, "Delete task")
           )
-        })
-      ),
+        )
+      })
+    ),
 
-      el.div(
-        el.a({ href: "#/" }, "All"),
-        el.a({ href: "#/active" }, "Active"),
-        el.a({ href: "#/completed" }, "Completed")
-      ),
+    el.div(
+      el.a({ href: "#/" }, "All"),
+      el.a({ href: "#/active" }, "Active"),
+      el.a({ href: "#/completed" }, "Completed")
+    ),
 
-      $if(pendingCount,
-        () => el.div(text`There are ${pendingCount} items in your todo list.`),
-        () => el.div("There is nothing in your todo list")
-      )
+    $if(pendingCount,
+      () => el.div(text`There are ${pendingCount} items in your todo list.`),
+      () => el.div("There is nothing in your todo list")
     )
   );
 }
